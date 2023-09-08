91095ac7dad98b68a414b0a85f552a8cd2f9557b
RihamMohamed101 <143026422+RihamMohamed101@users.noreply.github.com>
2023-09-08 11:28:20 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+
+//1
+let numberOne = 10 , numberTwo = 20 ;
+console.log(numberOne +""+numberTwo);
+console.log(typeof(numberOne +""+numberTwo));
+
+console.log(`${numberOne}${numberTwo}`);
+console.log(typeof(`${numberOne}${numberTwo}`));
+
+console.log(numberOne +"\n" +numberTwo)
+
+console.log(`${numberOne}
+${numberTwo}`);
+
+
+//2
+
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
+
+
+//3
+console.log("`I'am In" + "\n" + "\\\\"+ " \n" +"Love \\\\ \"\"\" ''' "
++"\n" + "++ with ++"+ "\n" + "\\\"\"\"\\\"\"\"" + "\n" +"\"\"javascript\"\"``");
+
+//4
+
+let a = 21;
+let b = 20;  // _21_2021_2021_2021_20_
+let c = b*100+a;
+
+console.log(`_${a}_${c}_${c}_${c}_${b}_`);
